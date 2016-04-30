eada47d95e75835771d8fdbc1e7bdf2858ed55b4
Luke Simmons <luke.simmons@vgregion.se>
2016-04-30 22:33:51 +0200

    Statistics looks better. Missing some gender columns

--- protected/controllers/StatistikController.php
+++ protected/controllers/StatistikController.php
@@ -10,14 +10,14 @@ class StatistikController extends Controller
 		$oCount->select="personnummer";
 		$count = Befolkningsinfo::model()->findAll($oCount);
 		$oWomen = new CDbCriteria();
-		$oCount->select="kon";
+		$oWomen->select="kon";
 		$oWomen->condition='kon=:women';
 		$oWomen->params=array(':women'=>'K');
 		$women = Befolkningsinfo::model()->findAll($oWomen);
 		$oMen = new CDbCriteria();
-		$oCount->select="kon";
-		$oWomen->condition='kon=:men';
-		$oWomen->params=array(':men'=>'M');
+		$oMen->select="kon";
+		$oMen->condition='kon=:men';
+		$oMen->params=array(':men'=>'M');
 		$men = Befolkningsinfo::model()->findAll($oMen);
 		
 		$this->render('view', array("count"=>$count, "women"=>$women, "men" => $men));
